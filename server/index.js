2a07f7fd7f503676b48bc499cf187e097a229ebf
Aryse Tansy <medictansy@gmail.com>
2022-11-08 18:05:03 -0500

    SERVER: Add Login Mock route for collab ref, and restClient

--- server/index.js
+++ server/index.js
@@ -18,6 +18,39 @@ app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 // DEV TESTING ROUTES HERE:
 
+app.post("/api/v1/login", (req, res) => {
+  const { username, password } = req.body;
+  const mockUsers = ["medic1111", "john", "brent", "lordson"];
+  const correctPass = "111111";
+
+  // THIS IS THE FIRST CHECK:
+
+  if (!username || !password) {
+    return res.status(422).json({ message: "All fields are required" });
+  }
+
+  // HERE WE USE MOCK DATA, BUT IT WILL BE A SEARCH ON DB
+  // IF SEARCH FAILS:
+
+  if (!mockUsers.includes(username)) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  // IF SEARCH DOESNT FAIL, WE BCRYPT COMPARE PASSWORD
+  // IF COMPARISON FAILS:
+
+  if (password !== correctPass) {
+    return res.status(403).json({ message: "Wrong password" });
+  }
+
+  // IF ALL CHECKS, USER CAN BE LOGGED IN:
+
+  res.status(200).json({ message: "Logging user" });
+
+  // USE THE AUTH.REST FILE TO SEND REQUESTS AND OBSERVE
+  // RESPONSE. THAT WILL BE THE FIRST LINE OF TESTING
+});
+
 app.get("/api", (req, res) =>
   res.status(200).json({ message: "Hello from server" })
 );
