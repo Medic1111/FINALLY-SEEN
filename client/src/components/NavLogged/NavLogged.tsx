6dc735ea27efbb7e85e18bfdb8592a2130eea531
Aryse Tansy <medictansy@gmail.com>
2022-11-09 13:45:24 -0500

    Component: NavLogged jsx ready for collab

--- client/src/components/NavLogged/NavLogged.tsx
+++ client/src/components/NavLogged/NavLogged.tsx
@@ -1,1 +1,24 @@
 import classes from "./NavLogged.module.css";
+
+const NavLogged: React.FC = () => {
+  return (
+    <nav>
+      {
+        // Write you JSX here
+      }
+    </nav>
+    // This nav will have element 'top'
+    // element 'top' will be an icon on screen smaller than 798px
+    // This nav will have element 'latest'
+    // element 'latest' will be an icon on screen smaller than 798px
+    // This nav will have element 'logout'
+    // element will be an icon on screen smaller than 798px
+    // USE MEDIA QUERY TO ACCOMPLISH TASKS
+    // Choose icons at your will from FONTS GOOGLE ICONS
+    // Dont forget to link the icons CDN on html
+    // STYLE ELEMENTS TO MATCH APP STYLING
+    // HAVE FUN and delete this comments when done
+  );
+};
+
+export default NavLogged;
