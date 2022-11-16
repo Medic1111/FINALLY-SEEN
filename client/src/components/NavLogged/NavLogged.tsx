9dd44fc0e4e054196d494df0da2dd0b7fdc1cec0
YahliGitzi <yahligitzi2@gmail.com>
2022-11-16 08:31:15 +0200

    feat(NavLogged): add icons

--- client/src/components/NavLogged/NavLogged.tsx
+++ client/src/components/NavLogged/NavLogged.tsx
@@ -19,19 +19,27 @@ const NavLogged: React.FC = () => {
         <li>
           <a href="https://github.com/Medic1111/FINALLY-SEEN">
             <span className={`${classes.navLink} ${classes.text}`}>Top</span>
-            <span className={`${classes.navLink} ${classes.icon}`}>i</span>
+            <span className={`${classes.navLink} ${classes.icon}`}>
+              <span className="material-symbols-outlined">
+                keyboard_double_arrow_up
+              </span>
+            </span>
           </a>
         </li>
         <li>
           <a href="https://github.com/Medic1111/FINALLY-SEEN">
             <span className={`${classes.navLink} ${classes.text}`}>Latest</span>
-            <span className={`${classes.navLink} ${classes.icon}`}>i</span>
+            <span className={`${classes.navLink} ${classes.icon}`}>
+              <span className="material-symbols-outlined">fiber_new</span>
+            </span>
           </a>
         </li>
         <li>
           <a href="https://github.com/Medic1111/FINALLY-SEEN">
             <span className={`${classes.navLink} ${classes.text}`}>Logout</span>
-            <span className={`${classes.navLink} ${classes.icon}`}>i</span>
+            <span className={`${classes.navLink} ${classes.icon}`}>
+              <span className="material-symbols-outlined">logout</span>
+            </span>
           </a>
         </li>
       </ul>
