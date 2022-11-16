d16a216fac38971f7d259ce42af088274e1d8314
YahliGitzi <yahligitzi2@gmail.com>
2022-11-16 08:34:58 +0200

    fix(NavLogged): remove comments

--- client/src/components/NavLogged/NavLogged.tsx
+++ client/src/components/NavLogged/NavLogged.tsx
@@ -1,17 +1,5 @@
 import classes from "./NavLogged.module.css";
 
-// This nav will have element 'top'
-// element 'top' will be an icon on screen smaller than 798px
-// This nav will have element 'latest'
-// element 'latest' will be an icon on screen smaller than 798px
-// This nav will have element 'logout'
-// element will be an icon on screen smaller than 798px
-// USE MEDIA QUERY TO ACCOMPLISH TASKS
-// Choose icons at your will from FONTS GOOGLE ICONS
-// Dont forget to link the icons CDN on html
-// STYLE ELEMENTS TO MATCH APP STYLING
-// HAVE FUN and delete this comments when done
-
 const NavLogged: React.FC = () => {
   return (
     <nav>
