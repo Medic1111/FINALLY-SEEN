85d3811888ebcbdf2c67e0587599bd6a2f40a461
aruadecarvalho <aruadecarvalho@gmail.com>
2022-11-20 09:28:20 -0300

    Issue#36 hamburguer menu component

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -1,5 +1,7 @@
 // ICONMONSTR
 
+import Hamburger from "../Hamburguer/Hamburger";
+
 interface Props {
   setShowNav: React.Dispatch<React.SetStateAction<boolean>>;
   setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
@@ -74,30 +76,7 @@ const Nav: React.FC<Props> = ({ setShowNav, setShowModal, setIsLoggin }) => {
             </p>
           </div>
         </div>
-
-        <div className="block md:hidden">
-          <button
-            onClick={() => {
-              setShowNav(true);
-            }}
-            className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              stroke-width="2"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </button>
-        </div>
+        <Hamburger setShowNav={setShowNav} />
       </div>
     </div>
   );
