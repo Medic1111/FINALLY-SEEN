ab62f8250d1632ca2b992ac260fa4178cfba1b0c
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-11-25 15:32:50 +0100

    feat:Extract the stat component from who component and use the teamdata model

--- client/src/components/Who/Who.tsx
+++ client/src/components/Who/Who.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Team from "../Team/Team";
 import { TeamData } from "../../models/TeamData";
+import Stats from "../Stats/Stats";
 
 const Who: React.FC = () => {
   const [data, setData] = useState<TeamData>({
@@ -39,50 +40,7 @@ const Who: React.FC = () => {
   return (
     <>
       <section id="who" className="bg-white">
-        <div className="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 md:py-16 lg:px-8">
-          <div className="mx-auto max-w-3xl text-center">
-            <p className="text-3xl font-bold text-gray-700 sm:text-4xl">Who</p>
-
-            <p className="mt-4 text-gray-600 sm:text-xl">
-              Dynamic team of developers at all levels, always evolving but most
-              importantly always counting.
-            </p>
-          </div>
-
-          <div className="mt-8 sm:mt-12">
-            <dl className="grid grid-cols-1 gap-4  sm:grid-cols-3">
-              <div className="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center">
-                <dt className="order-last text-lg font-medium text-gray-500">
-                  Collaborators
-                </dt>
-
-                <dd className="text-4xl font-extrabold text-teal-500 md:text-5xl">
-                  {data.collaborators}
-                </dd>
-              </div>
-
-              <div className="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center">
-                <dt className="order-last text-lg font-medium text-gray-500">
-                  Commits
-                </dt>
-
-                <dd className="text-4xl font-extrabold text-teal-500 md:text-5xl">
-                  {data.commits}
-                </dd>
-              </div>
-
-              <div className="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center">
-                <dt className="order-last text-lg font-medium text-gray-500">
-                  Team {"=)"}
-                </dt>
-
-                <dd className="text-4xl font-extrabold text-teal-500 md:text-5xl">
-                  1
-                </dd>
-              </div>
-            </dl>
-          </div>
-        </div>
+        <Stats data={data} />
         <Team data={data} />
       </section>
     </>
