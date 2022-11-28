42f384dde04c23dd93f1524e08a12b7da4bd9084
Valan Baptist Mathuranayagam <valanm22@gmail.com>
2022-11-28 22:25:03 +0530

    Fix lint errors

--- client/src/components/NavMobile/NavMobile.tsx
+++ client/src/components/NavMobile/NavMobile.tsx
@@ -27,9 +27,9 @@ const NavMobile: React.FC<Props> = ({ setShowNav }) => {
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M6 18L18 6M6 6l12 12"
                   />
                 </svg>
--- client/src/pages/Portfolio.tsx
+++ client/src/pages/Portfolio.tsx
@@ -51,11 +51,11 @@ const Portfolio: React.FC = () => {
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="teal"
-                stroke-width="2"
+                strokeWidth="2"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M23.245 20l-11.245-14.374-11.219 14.374-.781-.619 12-15.381 12 15.391-.755.609z"
                 />
               </svg>
@@ -68,11 +68,11 @@ const Portfolio: React.FC = () => {
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="teal"
-                stroke-width="2"
+                strokeWidth="2"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M4 .755l14.374 11.245-14.374 11.219.619.781 15.381-12-15.391-12-.609.755z"
                 />
               </svg>
