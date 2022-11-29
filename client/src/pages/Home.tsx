491bbc766d7584fe51908f306ea685e39a8f1f06
augustinekirumba <steentoonz@gmail.com>
2022-11-29 16:11:06 +0300

    Adding the modal functionality on button click

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,13 +1,19 @@
+import React from "react";
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
 import Who from "../components/Who/Who";
 import Why from "../components/Why/Why";
 
-const Home: React.FC = () => {
+interface Props {
+  setShowModal: React.Dispatch<React.SetStateAction<boolean>>
+  setIsLoggin: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const Home: React.FC<Props> = ({setShowModal, setIsLoggin}) => {
   return (
     <>
       {/* <Header /> */}
-      <Hero />
+      <Hero setShowModal={setShowModal} setIsLoggin={setIsLoggin} />
       <Who />
       <Why />
     </>
